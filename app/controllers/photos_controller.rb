96ef60cd0bc514816fa418a4532c86026f78940d
Griffin Cox <griffincox@users.noreply.github.com>
2017-02-06 11:12:38 -0600

    100%

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -16,8 +16,49 @@ class PhotosController < ApplicationController
     @photo = Photo.find(@photo_id)
     @photo.destroy
     @list_of_photos = Photo.all
-    render("index.html.erb")
+    redirect_to("http://localhost:3000/photos")
   end
 
+  def new_form
+    render("new_form.html.erb")
+  end
+
+  def create_row
+    @photo_url = params[:the_source]
+    @photo_caption = params[:the_caption]
+    @new_photo = Photo.new
+    @new_photo.source = @photo_url
+    @new_photo.caption = @photo_caption
+    if (@new_photo.save)
+      @list_of_photos = Photo.all
+      redirect_to("http://localhost:3000/photos")
+    else
+      render("new_form.html.erb")
+    end
+
+  end
+
+  def edit_form
+    @photo_id = params[:id]
+    @photo = Photo.find(@photo_id)
+    @photo_url = @photo.source
+    @photo_caption = @photo.caption
+    render("edit_form.html.erb")
+  end
+
+  def update_row
+    @photo_id = params[:id]
+    @photo_url = params[:the_source]
+    @photo_caption = params[:the_caption]
+    @update_photo = Photo.find(@photo_id)
+    @update_photo.source = @photo_url
+    @update_photo.caption = @photo_caption
+    if (@update_photo.save)
+      @list_of_photos = Photo.all
+      redirect_to("http://localhost:3000/photos/#{@photo_id}")
+    else
+      render("edit_form.html.erb")
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  # CREATE
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
-  # CREATE
-
   # UPDATE
-
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
   # DESTROY
   get("/delete_photo/:id", { :controller => "photos", :action => "delete_photo" })
 end
